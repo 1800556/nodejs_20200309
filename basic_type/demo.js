042f521f46fdd784fd3f6b60d15d2e2dd795cd2b
1800556 <joqiu@systex.com>
2020-03-10 15:03:48 +0800

    finish basic type

--- basic_type/demo.js
+++ basic_type/demo.js
@@ -135,7 +135,7 @@ console.log(x2, j2)
 var o1 = JSON.parse(j1)
 var o2 = JSON.parse(j2)
 console.log(Array.isArray(o1), typeof o1, o1)
-console.log(Array.isArray(o2), typeof o2, o2)*/
+console.log(Array.isArray(o2), typeof o2, o2)
 
 var a1 = ['a', 'b', 'c', 'd', 'e', 'f']
 var a2 = Array.from('abcdef')
@@ -156,3 +156,153 @@ console.log(a1)
 console.log(a2.shift(), a2)
 a2.unshift('A')
 console.log(a2)
+
+var a1 = [1, 2, 3, 4, 5, 6, 7, 8]
+var r1 = a1.join()
+console.log(typeof r1, r1)
+var a2 = ['red', 'yellow', 'blue']
+var r2 = a2.join()
+console.log(typeof r2, r2)
+var r3 = a2.join('-')
+console.log(r3)
+var a3 = ['www', 'uuu', 'com', 'tw']
+var r4 = a3.join('.')
+console.log(r4)
+array4 = Array.from('HelloWorld')
+console.log(array4.sort())
+var sum = 0
+a1.forEach(function (element) { sum += element }, this)
+console.log("summation of a1=", sum)
+
+var util = require('util')
+console.log(123 == '123', 25.4 == '25.4', 0 == '0')
+console.log(123 === '123', 25.4 === '25.4', 0 === '0')
+var a1 = [null, undefined, 0, false, NaN, '']
+for (i = 0; i < a1.length; i++) {
+    for (j = i + 1; j < a1.length; j++) {
+        result = util.format('%s == %s? %s', a1[i], a1[j], a1[i] == a1[j])
+        console.log(result)
+        //console.log('is', a1[i], '===', a1[j], '==>',a1[i] === a1[j])
+        console.log('------------')
+    }
+}
+
+var x = 123
+var y = new Number(123)
+console.log(typeof x, typeof y)
+console.log(x == y, x === y)
+
+function echoMe(name) {
+    console.log("echo name:",name)
+}
+echoMe("Mark")
+echoMe()
+echoMe(undefined)
+echoMe(null)
+echoMe('p','q','r')
+echoMe(['p','q','r'])
+
+function mySum() {
+    var sum = 0
+    for (var i = 0; i < arguments.length; i++) {
+        sum += arguments[i]
+    }
+    return sum
+}
+console.log(mySum())
+console.log(mySum(1, 2, 3))
+console.log(mySum('1', '2', '3'))
+var x1 = function (a, b) {
+    return a * b
+}
+console.log(x1(3, 5))
+
+var token = 'abc'
+var y = function () {
+    var token = 'def'
+    console.log('inside', token)
+}
+console.log('first outside', token)
+y()
+console.log('second outside', token)
+var x1 = function (x, y) {
+    return x * y
+}
+var x2 = (x, y) => { return x * y }
+var x3 = (x, y) => x * y
+console.log(x1(3, 4))
+console.log(x2(6, 7))
+console.log(x3(8, 9))
+
+var x1 = [null, undefined, 0, false, "", NaN]
+x1.forEach(e => console.log(e ? "true" : "false"))
+var x2 = [2, 4, 6, 8]
+for (index in x2) {
+    console.log('index=', index, " ,value=", x2[index])
+}
+for (item of x2) {
+    console.log('value=', item)
+}
+var x3 = {
+    courseName: "NoJS",
+    duration: 14,
+    location: "Taipei"
+}
+for (k in x3) {
+    console.log(k, "/", x3[k])
+}
+
+function Car() {
+    // field
+    this.speed = 0
+    this.number = ""
+    this.fuel = 0
+    // method
+    this.setSpeed = function (speed) { this.speed = speed }
+    this.isOverSpeed = function (speedLimit) {
+        return this.speed > speedLimit
+    }
+}
+var myCar1 = new Car()
+console.log(typeof myCar1, typeof Car)
+myCar1.setSpeed(60)
+myCar1.number = "AAA-1234"
+console.log(myCar1.number + " has speed:" + myCar1.speed)
+console.log(myCar1.isOverSpeed(100), myCar1.isOverSpeed(50))
+*/
+function Car() {
+}
+// field
+Car.prototype.speed = 0
+Car.prototype.number = ""
+Car.prototype.fuel = 0
+// method
+Car.prototype.setSpeed = function (speed) { Car.prototype.speed = speed }
+Car.prototype.isOverSpeed = function (speedLimit) {
+    return Car.prototype.speed > speedLimit
+}
+function HybridCar() {
+
+}
+HybridCar.prototype = new Car()
+HybridCar.prototype.__proto__ = Car.prototype
+HybridCar.prototype.batteryLimit = 50000
+HybridCar.prototype.totalLength = function () {
+    return this.fuel * 21 + this.batteryLimit / 500
+}
+
+var myCar1 = new Car()
+console.log(typeof myCar1, typeof Car)
+myCar1.setSpeed(60)
+myCar1.number = "AAA-1234"
+console.log(myCar1.number + " has speed:" + myCar1.speed)
+console.log(myCar1.isOverSpeed(100), myCar1.isOverSpeed(50))
+//
+var myCar2 = new HybridCar()
+console.log(typeof myCar2, typeof HybridCar)
+myCar2.setSpeed(60)
+myCar2.number = "DDD-5678"
+console.log(myCar2.number + " has speed:" + myCar2.speed)
+console.log(myCar2.isOverSpeed(100), myCar2.isOverSpeed(50))
+myCar2.fuel = 70
+console.log("myCar2 total length=",myCar2.totalLength())
